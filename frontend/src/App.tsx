56db0a00d387a3a7346abe1c93e4823fd0475ab1
jeovane16 <jeovanearaujo16@gmail.com>
2020-07-20 19:07:13 -0300

    estrutura da pagina com css grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import CardGame from './components/CardGame/CardGame';
 import Cart from './components/Cart/Cart';
 
+import getProducts from './services/api';
+
+interface Products {
+  id: number,
+  name: string,
+  price: number,
+  score: number,
+  image: string
+}
+
 function App() {
+
+  const [products, setProducts] = useState<Products[]>([]);
+
+  useEffect(()=>{
+    getProducts().then(response =>{
+      const newProducts = response;
+      setProducts(newProducts);
+    })
+  });
+
+  console.log(products)
+
   return (
-    <div>
-      <Header/>
-      <CardGame/>
+    <div className='App'>
+      <Header />
+      <main className='containerMain'>
+        {products.map(product =>
+            <CardGame product={product}/>
+        )}
+      </main>
       <Cart />
     </div>
   );
--- frontend/src/components/CardGame/CardGame.tsx
+++ frontend/src/components/CardGame/CardGame.tsx
@@ -2,16 +2,26 @@ import React, { useState } from 'react';
 import './CardGame.css';
 import image from '../../assets/fifa-18.png';
 
-export default function CardGame(){
+interface Products {
+    id: number,
+    name: string,
+    price: number,
+    score: number,
+    image: string
+  }
+
+export default function CardGame(props: {product: Products}){
+
+    const formatPrice = props.product.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 
     return (
         <div className='containerCardGame'>
             <div className='image'>
-                <img src={image} alt="teste"/>
+                <img src={require(`../../assets/${props.product.image}`)} alt={props.product.name}/>
             </div>
             <div className='infoGame'>
-                <span className='nameGame'>FIFA 18</span>
-                <span className='priceGame'>R$ 195,39</span>
+                <span className='nameGame'>{props.product.name}</span>
+                <span className='priceGame'>{formatPrice}</span>
             </div>
             <button>Adicionar ao carrinho</button>
         </div>
